ef9972a08d267ca70517f7b72d6ec101a2ce1c61
Dan Kreiger <dan@dankreiger.com>
2021-03-08 23:42:01 +0100

    chore: try function composition

--- src/Main.hs
+++ src/Main.hs
@@ -1,9 +1,28 @@
-countReducer :: Integer -> [Char] -> Integer
-countReducer state actionType  
+-- inside
+counter :: Integer -> [Char] -> Integer
+counter state actionType
   | actionType == "increment" = state + 1
   | actionType == "decrement" = state - 1
   | otherwise   = state
 
 
+increment :: Integer -> Integer
+increment x = counter x "increment"
 
-   
+decrement :: Integer -> Integer
+decrement x = counter x "decrement"
+
+
+---------------------------------------
+---------------------------------------
+---------------------------------------
+
+
+-- outside
+main :: IO ()
+
+main = do
+  if ans == 7
+    then print "passed"
+    else error ( "must evaluate to 7. Got: " ++ show ans )
+  where ans = ( increment . increment . decrement . increment ) 5
